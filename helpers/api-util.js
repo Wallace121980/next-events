51edc7abe561fa024dd9a5e0e19625731bece682
Wallace121980 <williamschrauwen@gmail.com>
2021-05-12 19:02:46 +0200

    Added data to firebase and static site generation to homepage

---
+++ helpers/api-util.js
@@ -0,0 +1,22 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    'https://nextjs-course-c2b67-default-rtdb.europe-west1.firebasedatabase.app/events.json'
+  );
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
